b874dc91baa54b517d1596bac433f2e62b87192c
carmopi <carmopi1994@gmail.com>
2021-02-26 13:22:16 +0100

    add named query defined

--- src/main/java/com/carmen/app/entities/Car.java
+++ src/main/java/com/carmen/app/entities/Car.java
@@ -10,6 +10,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 //import javax.persistence.Temporal;
 //import javax.persistence.TemporalType;
@@ -20,6 +21,7 @@ import org.hibernate.annotations.UpdateTimestamp;
 
 @Entity
 @Table(name = "cars")
+@NamedQuery(name = "FindCars", query ="SELECT a FROM Car a")
 public class Car implements Serializable {
 	
 	
