1ed2873d6be06a9399e816db6f6d3c892faf3f43
carmopi <carmopi1994@gmail.com>
2021-03-05 11:36:58 +0100

    fix code style

--- src/main/java/com/carmen/app/boundary/ICarResource.java
+++ src/main/java/com/carmen/app/boundary/ICarResource.java
@@ -16,65 +16,33 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
 public interface ICarResource {
 
-	@Operation(summary = "Get all cars",
-			responses = {
-					@ApiResponse(responseCode = "200",
-							description = "Get all cars",
-							content =  @Content(mediaType = "application/json",
-							schema = @Schema(implementation = Car.class)))
-			})
-public List<Car> getCars() ;
-	
-	@Operation(summary = "Get car by id",
-			responses = {
-					@ApiResponse(responseCode = "200",
-							description = "Get car by id",
-							content = @Content(mediaType = "application/json",
-							schema = @Schema(implementation = Car.class))),
-					@ApiResponse(responseCode = "404",
-								description = "There is no car with this id")
-			})
-public Response getCar(@Parameter(description = "Car id", required = true) UUID id);
-	
-	@Operation(summary = "Update a car",
-			responses = {
-					@ApiResponse(responseCode = "201",
-							description = "Car update", 
-							content = @Content(mediaType = "application/json",
-							schema = @Schema(implementation = Car.class))),
-						@ApiResponse(responseCode = "400",
-							description = "Car not valid"),
-						@ApiResponse(responseCode = "404",
-									description = "There is no car with this id")	
-			})
-public Response updateCar(@Parameter(description = "Car that needs to be updated", required = true) UUID id);
-	
-	@Operation(summary = "Create a new Car",      
-			responses = {
-					@ApiResponse(responseCode = "201",
-							description = "Car created",
-							content= @Content(mediaType = "application/json",
-							schema = @Schema(implementation = Car.class))),
-					@ApiResponse(responseCode = "400",    
-								description = "Car not valid")
-					
-							
-							
-})
-public Response createdCar(@RequestBody(description = "Created a new Car", required = true,
-								content = @Content(schema = @Schema(implementation = Car.class))) Car car);
-	
-	@Operation(summary = "Delete a Car",
-			responses =  {
-					@ApiResponse(responseCode = "204", 
-							description = "The car has been deleted", 
-							content = @Content(mediaType = "application/json", 
-							schema = @Schema(implementation = Car.class))),
-					@ApiResponse(responseCode = "404", 
-							description = "There is no car with this id"),	
-					@ApiResponse(responseCode = "400",    
-							description = "Car not valid")
-			})
-public Response deleteCar(@Parameter(description = "Car that need to be deleted", required = true) UUID id);
-	
+	@Operation(summary = "Get all cars", responses = {
+			@ApiResponse(responseCode = "200", description = "Get all cars", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Car.class))) })
+	public List<Car> getCars();
+
+	@Operation(summary = "Get car by id", responses = {
+			@ApiResponse(responseCode = "200", description = "Get car by id", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Car.class))),
+			@ApiResponse(responseCode = "404", description = "There is no car with this id") })
+	public Response getCar(@Parameter(description = "Car id", required = true) UUID id);
+
+	@Operation(summary = "Update a car", responses = {
+			@ApiResponse(responseCode = "201", description = "Car update", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Car.class))),
+			@ApiResponse(responseCode = "400", description = "Car not valid"),
+			@ApiResponse(responseCode = "404", description = "There is no car with this id") })
+	public Response updateCar(@Parameter(description = "Car that needs to be updated", required = true) UUID id);
+
+	@Operation(summary = "Create a new Car", responses = {
+			@ApiResponse(responseCode = "201", description = "Car created", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Car.class))),
+			@ApiResponse(responseCode = "400", description = "Car not valid")
+
+	})
+	public Response createdCar(
+			@RequestBody(description = "Created a new Car", required = true, content = @Content(schema = @Schema(implementation = Car.class))) Car car);
+
+	@Operation(summary = "Delete a Car", responses = {
+			@ApiResponse(responseCode = "204", description = "The car has been deleted", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Car.class))),
+			@ApiResponse(responseCode = "404", description = "There is no car with this id"),
+			@ApiResponse(responseCode = "400", description = "Car not valid") })
+	public Response deleteCar(@Parameter(description = "Car that need to be deleted", required = true) UUID id);
+
 }
